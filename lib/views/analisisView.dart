1966e0abc7a77ae1e1b5ebf9765b84c086fe5e9f
Juanpivl <juanpivl@hotmail.com>
2024-02-19 16:33:10 -0700

    comienzo para exportar excel

--- lib/views/analisisView.dart
+++ lib/views/analisisView.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:money/models/Movimiento.dart';
-import 'package:money/services/categoria_provider.dart';
 import 'package:money/services/movimiento_provider.dart';
+import 'package:money/services/categoria_provider.dart';
+import 'package:money/models/Movimiento.dart';
 import 'package:money/components/customAppBar.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:excel/excel.dart';
+import 'package:share/share.dart';
 
 class AnalisisView extends StatefulWidget {
   const AnalisisView({Key? key}) : super(key: key);
@@ -19,132 +23,91 @@ class _AnalisisViewState extends State<AnalisisView> {
   @override
   Widget build(BuildContext context) {
     final categoriaProvider = Provider.of<CategoriaProvider>(context);
-    final List<String> categorias = ['Todos'] +
-        categoriaProvider.categorias
-            .map((categoria) => categoria.nombre)
-            .toList();
-
     final movimientoProvider = Provider.of<MovimientoProvider>(context);
-    final List<Movimiento> movimientos = movimientoProvider.movimientos;
 
-    List<Movimiento> movimientosFiltrados = movimientos.where((movimiento) {
+    List<String> categorias =
+        ['Todos'] + categoriaProvider.categorias.map((c) => c.nombre).toList();
+    List<String> tiposDeMovimiento = ['Todos', 'Ingreso', 'Egreso'];
+
+    List<Movimiento> movimientosFiltrados =
+        movimientoProvider.movimientos.where((movimiento) {
       bool matchCategoria = _selectedCategoria == 'Todos' ||
           movimiento.categoria == _selectedCategoria;
-      bool matchTipoMovimiento = _selectedTipoMovimiento == 'Todos' ||
+      bool matchTipo = _selectedTipoMovimiento == 'Todos' ||
           movimiento.tipo == _selectedTipoMovimiento;
-      return matchCategoria && matchTipoMovimiento;
+      return matchCategoria && matchTipo;
     }).toList();
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.deepPurple[50],
         appBar: CustomAppBar(showTitle: true, titleText: 'Análisis'),
-        body: Container(
-          margin: const EdgeInsets.all(10),
-          child: Column(
-            children: [
-              Expanded(
-                  child: Container(
-                child: Text('aqui unas graficas bien mamalonas'),
-              )),
-              Expanded(
-                child: movimientosFiltrados.isNotEmpty
-                    ? ListView.builder(
-                        itemCount: movimientosFiltrados.length,
-                        itemBuilder: (context, index) {
-                          final movimiento = movimientosFiltrados[index];
-                          return ListTile(
-                            title: Text(
-                                "${movimiento.tipo}: \$${movimiento.cantidad}"),
-                            subtitle: Text(
-                                "${movimiento.categoria} - ${movimiento.concepto}"),
-                          );
-                        },
-                      )
-                    : Center(child: Text("No hay movimientos para mostrar")),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: movimientosFiltrados.length,
+                itemBuilder: (context, index) {
+                  Movimiento movimiento = movimientosFiltrados[index];
+                  return ListTile(
+                    title: Text("${movimiento.tipo}: \$${movimiento.cantidad}"),
+                    subtitle: Text(
+                        "${movimiento.categoria} - ${movimiento.concepto}"),
+                  );
+                },
               ),
-              SizedBox(height: 10),
-              _filtros(categorias),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: DropdownButton<String>(
+                    value: _selectedCategoria,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedCategoria = newValue!;
+                      });
+                    },
+                    items: categorias
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.download),
+                  onPressed: () => descargarExcel(movimientosFiltrados),
+                ),
+                Expanded(
+                  child: DropdownButton<String>(
+                    value: _selectedTipoMovimiento,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedTipoMovimiento = newValue!;
+                      });
+                    },
+                    items: tiposDeMovimiento
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Row _filtros(List<String> categorias) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          width: 140,
-          margin: EdgeInsets.all(5),
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-          decoration: BoxDecoration(
-            color: Colors.blueAccent,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              isExpanded: true,
-              value: _selectedCategoria,
-              dropdownColor: Colors.blueAccent,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedCategoria = newValue!;
-                });
-              },
-              items: categorias.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.deepPurple, borderRadius: BorderRadius.circular(5)),
-          height: 50,
-          width: 50,
-          margin: EdgeInsets.all(5),
-          child: GestureDetector(
-            child: Icon(Icons.download),
-            onTap: () {
-              print('descargando');
-            },
-          ),
-        ),
-        Container(
-          width: 140,
-          margin: EdgeInsets.all(5),
-          padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-          decoration: BoxDecoration(
-              color: Colors.blueAccent, borderRadius: BorderRadius.circular(5)),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              isExpanded: true,
-              dropdownColor: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
-              value: _selectedTipoMovimiento,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedTipoMovimiento = newValue!;
-                });
-              },
-              items: ['Todos', 'Ingreso', 'Egreso']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-      ],
-    );
+  Future<void> descargarExcel(List<Movimiento> movimientos) async {
+    // Implementa la lógica para descargar Excel aquí
+    print('excel');
   }
 }
